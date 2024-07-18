78eb7ca655048a46b20ab5653fef9ca979dda13f
Priyal Soni <priyalsoni46@gmail.com>
2024-07-18 10:47:11 +0530

    Add files via upload

---
+++ auto.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+auto g= 8;
+int main()
+{
+	auto a =5;
+	auto ptr = &x;
+	cout<<g;
+}
---
+++ nullpointer.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	int *ptr = nullptr;
+	return 0;
+}
