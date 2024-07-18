31583ff63e6752f1f247304fb2019bcc2f6ce583
Priyal Soni <priyalsoni46@gmail.com>
2024-07-18 10:49:43 +0530

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int  x =5;
+	const int y =5;
+	
+	auto *px1 = &x;
+	auto px2 =&x;
+}
---
+++ reference.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int x =5;
+	int &reference  = x;
+	
+	cout<<"x = "<< x<< endl;
+	cout<<"reference = " <<reference <<endl;
+	
+	}
+
